2ec7eac967596a9c11363e1ddfce73e07369f48f
yoi3.0 <dkurpachenko@gmail.com>
2020-04-03 17:28:43 +0300

    Until multiply done (st2)

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@ public:
         return this->get().length();
     }
     friend Natur operator+(Natur& left, Natur& right);
+    friend Natur operator-(Natur& left, Natur& right);
+    friend int COM_NN_D(Natur a, Natur b);
 };
 Natur operator+(Natur& left,Natur& right){
     string res,res2;
@@ -54,6 +56,31 @@ Natur operator+(Natur& left,Natur& right){
     max.init(res2);
     return max;
 }
+Natur operator-(Natur& left, Natur& right)
+{
+    string a,b,rez="";
+    int x,i;
+    Natur res;
+    if ((x=COM_NN_D(left,right))==2) {a=left.get(); b=right.get();}
+    else { if (x==0) {res.init("0"); return res;} a=right.get(); b=left.get();}
+    for (i=0;i<a.size()-b.size();i++) rez+=a[i];
+    for (;i<a.size();i++) {
+        if ((a[i] - '0') - (b[i-(a.size()-b.size())] - '0') < 0) {
+            for (int j = i - 1; j >= 0; j--) {
+                //cout << "debug";
+                if ((rez[j] - '0') > 0) {
+                    rez[j] = (char) (rez[j] - 1);
+                    for (int z = j+1; z <= i - 1; z++) rez[z] = '9';
+                    break;
+                }
+            }
+            rez += (char) ((10 + a[i] - '0') - (b[i-(a.size()-b.size())] - '0') + '0');
+        } else rez += (char) ((a[i] - '0') - (b[i-(a.size()-b.size())] - '0') + '0');
+    }
+    for (i=0;i<rez.length();i++) if (rez[i]=='0') rez.erase(i,1);
+    res.init(rez);
+    return res;
+}
 /*
  * Дополнительные функции
  */
@@ -65,12 +92,12 @@ int error(int type){  // вывод ошибки
  * Функции основной работы
  */
 
-int COM_NN_D(Natur a, Natur b) //1 if 1e>; 2 if <; 3 if ==
+int COM_NN_D(Natur a, Natur b) //2 if 1e>; 1 if <; 0 if ==
 {
-    if (a.size()>b.size()) return 1; else if (a.size()<b.size()) return 2;
+    if (a.size()>b.size()) return 2; else if (a.size()<b.size()) return 1;
     else for (int i=0;i<a.size();i++)
-        if (a.get()[i]>b.get()[i]) return 1; else  if (a.get()[i]<b.get()[i]) return 2;
-    return 3;
+        if (a.get()[i]>b.get()[i]) return 2; else  if (a.get()[i]<b.get()[i]) return 1;
+    return 0;
 }
 int NZER_N_B(Natur a) //0 if ==0, 1 if !=0
 {
@@ -80,10 +107,36 @@ string ADD_1N_N(Natur a){
     Natur a2("1");
     return (a+a2).get();
 }
+string ADD_NN_N(Natur a, Natur b){
+    return (a+b).get();
+}
+string SUB_NN_N(Natur a, Natur b){
+    return (a-b).get();
+}
+string MUL_ND_N(Natur a, int b){
+    string res="",rez="";
+    int x=0; //promejutochniy result
+    for (int i=a.size()-1;i>=0;i--)
+    {
+        x+=(a.get()[i]-'0')*b;
+        res+=(char)(x%10+'0');
+        x/=10;
+    }
+    if (x!=0) res+=(char)(x+'0'); //result<=81 that's why it should work (c) yoi3.0
+    for (int i=0;i<res.length();i++)
+        rez+=res[res.length()-1-i];
+    return rez;
+}
+string MUL_Nk_N(Natur a, int k)
+{
+    string res=a.get();
+    for (int i=0;i<k;i++) res+='0';
+    return res;
+}
 int naturalis() //работаем с натуральными
 {
     Natur n1, n2;
-    int res, isOk;
+    int res, isOk,mnoj;
     string resStr;
     cout<<"Введите два натуральных числа";
     n1.init(); n2.init();
@@ -94,12 +147,22 @@ int naturalis() //работаем с натуральными
     <<"11 - частное (большее/меньшее) с остатком\n"<<"12 - остаток от деления\n"<<"13 - НОД\n"<<"14 - НОК\n";
     cin>>funkType;
     switch (funkType){
-        case 1: if((res=COM_NN_D(n1,n2))==1) cout<<"Первое больше"; else res==2? cout<<"Второе больше": cout<<"Числа равны"; break;
+        case 1: if((res=COM_NN_D(n1,n2))==2) cout<<"Первое больше"; else res==1? cout<<"Второе больше": cout<<"Числа равны"; break;
         case 2: cout<<"Какое число сравниваем: 1е или 2е? (Введите цифру)\n";  cin>>res;
         if (res==1) res=NZER_N_B(n1); else if (res==2) res=NZER_N_B(n2); else return error(1);
         if (res==1) cout<<"Число не является нулём\n"; else cout<<"Число является нулём\n"; break;
         case 3: cout<<"Какое число сравниваем: 1е или 2е? (Введите цифру)\n";  cin>>res;
             if (res==1) resStr=ADD_1N_N(n1); else if (res==2) resStr=ADD_1N_N(n2); else return error(1);  cout<<"Получилось число "<<resStr<<'\n'; break;
+        case 4: cout<<"Получилось число "<<ADD_NN_N(n1, n2)<<'\n'; break;
+        case 5: cout<<"Результат вычитания "<<SUB_NN_N(n1,n2)<<'\n'; break;
+        case 6: cout<<"Какое число умножаем: 1е или 2е (Введите цифру)? Затем введите множитель (тип int)\n";  cin>>res; cin>>mnoj;
+            if (cin.fail() || mnoj<0) return error(1);
+            if (res==1) cout<<"Результат умножения "<<MUL_ND_N(n1, mnoj)<<'\n';
+            else if (res==2) cout<<"Результат умножения "<<MUL_ND_N(n2, mnoj)<<'\n'; else return error(1); break;
+        case 7: cout<<"Какое число умножаем: 1е или 2е (Введите цифру)? Затем введите степень k (тип int)\n";  cin>>res; cin>>mnoj;
+            if (cin.fail() || mnoj<0) return error(1);
+            if (res==1) cout<<"Результат умножения "<<MUL_Nk_N(n1, mnoj)<<'\n';
+            else if (res==2) cout<<"Результат умножения "<<MUL_Nk_N(n2, mnoj)<<'\n'; else return error(1); break;
         default: return error(1);
     }
     return 0;
